843047937836ed76974cd1af8afa5cc202bd7fbd
Prashan Raj KC <Prashan@codebee.dk>
2024-11-21 10:33:01 +0545

    #
    Refactor ShowNumber method for parameterized threading
    
    Modified ShowNumber to accept an object parameter maxNumber.
    Updated loop to iterate up to (int)maxNumber for dynamic limit.
    Replaced ThreadStart with ParameterizedThreadStart for threading.
    
    Refactor threading and add ShowNumber method
    
    Added ShowNumber method to Program class to print numbers 0-4.
    Modified Main method to remove initial and final console outputs.
    Commented out creation and starting of threads t1, t2, and t3.
    Introduced ThreadStart delegate with lambda for ShowNumber.
    Commented out Method1, Method2, and Method3 methods.

--- MultiThreadingAndParallelism/Program.cs
+++ MultiThreadingAndParallelism/Program.cs
@@ -5,73 +5,92 @@ namespace MultiThreadingInCSharp
 {
     class Program
     {
+
+        static void ShowNumber(object maxNumber)
+        {
+            for (int i = 0; i < (int)maxNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Main Thread Started");
 
-            //Creating Threads
-            Thread t1 = new (Method1)
-            {
-                Name = "Thread1"
-            };
+            //ThreadStart obj = new ThreadStart(ShowNumber);
+            //ThreadStart obj = ShowNumber;
 
-            Thread t2 = new (Method2)
-            {
-                Name = "Thread2"
-            };
+            //ThreadStart obj = () => ShowNumber(6);
 
-            Thread t3 = new (Method3)
-            {
-                Name = "Thread3"
-            };
-            //Executing the methods
-            t1.Start();
-            t2.Start();
-            t3.Start();
-            Console.WriteLine("Main Thread Ended");
+            ParameterizedThreadStart obj = new ParameterizedThreadStart(ShowNumber);
+            Thread t = new Thread(ShowNumber);
+            t.Start(10);
+            //ShowNumber();
+
+
+            ////Creating Threads
+            //Thread t1 = new(Method1)
+            //{
+            //    Name = "Thread1"
+            //};
+
+            //Thread t2 = new(Method2)
+            //{
+            //    Name = "Thread2"
+            //};
+
+            //Thread t3 = new(Method3)
+            //{
+            //    Name = "Thread3"
+            //};
+            ////Executing the methods
+            //t1.Start();
+            //t2.Start();
+            //t3.Start();
+            //Console.WriteLine("Main Thread Ended");
             Console.ReadLine();
         }
 
 
 
-        static void Method1()
-        {
+        //static void Method1()
+        //{
 
-            Console.WriteLine("Method1 Started using " + Thread.CurrentThread.Name);
+        //    Console.WriteLine("Method1 Started using " + Thread.CurrentThread.Name);
 
-            for (int i = 1; i <= 5; i++)
-            {
-                Console.WriteLine("Method1 :" + i);
-            }
-            Console.WriteLine("Method1 Ended using " + Thread.CurrentThread.Name);
-        }
+        //    for (int i = 1; i <= 5; i++)
+        //    {
+        //        Console.WriteLine("Method1 :" + i);
+        //    }
+        //    Console.WriteLine("Method1 Ended using " + Thread.CurrentThread.Name);
+        //}
 
-        static void Method2()
-        {
-            Console.WriteLine("Method2 Started using " + Thread.CurrentThread.Name);
-            for (int i = 1; i <= 5; i++)
-            {
-                Console.WriteLine("Method2 :" + i);
-                if (i == 2)
-                {
-                    Console.WriteLine("Method2 DB operation Started");
-                    //Sleep for 10 seconds
-                    Thread.Sleep(10000);
-                    Console.WriteLine("Method2 DB operation Completed");
-                }
-            }
-            Console.WriteLine("Method2 Ended using " + Thread.CurrentThread.Name);
-        }
+        //static void Method2()
+        //{
+        //    Console.WriteLine("Method2 Started using " + Thread.CurrentThread.Name);
+        //    for (int i = 1; i <= 5; i++)
+        //    {
+        //        Console.WriteLine("Method2 :" + i);
+        //        if (i == 2)
+        //        {
+        //            Console.WriteLine("Method2 DB operation Started");
+        //            //Sleep for 10 seconds
+        //            Thread.Sleep(10000);
+        //            Console.WriteLine("Method2 DB operation Completed");
+        //        }
+        //    }
+        //    Console.WriteLine("Method2 Ended using " + Thread.CurrentThread.Name);
+        //}
 
-        static void Method3()
-        {
-            Console.WriteLine("Method3 Started using " + Thread.CurrentThread.Name);
-            for (int i = 1; i <= 5; i++)
-            {
-                Console.WriteLine("Method3 :" + i);
-            }
-            Console.WriteLine("Method3 Ended using " + Thread.CurrentThread.Name);
-        }
+        //static void Method3()
+        //{
+        //    Console.WriteLine("Method3 Started using " + Thread.CurrentThread.Name);
+        //    for (int i = 1; i <= 5; i++)
+        //    {
+        //        Console.WriteLine("Method3 :" + i);
+        //    }
+        //    Console.WriteLine("Method3 Ended using " + Thread.CurrentThread.Name);
+        //}
     }
 
 
