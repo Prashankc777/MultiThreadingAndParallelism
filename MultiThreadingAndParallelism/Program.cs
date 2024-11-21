b91d655eec0912c6140a60bf692cb311ccc50fc4
Prashan Raj KC <Prashan@codebee.dk>
2024-11-21 09:42:13 +0545

    Add project files.

---
+++ MultiThreadingAndParallelism/Program.cs
@@ -0,0 +1,10 @@
+﻿//Console.WriteLine("Hello, World!");
+
+Thread t = Thread.CurrentThread;
+
+t.Name = "Main Thread";
+Console.WriteLine("Thread Name" + t.Name);
+Console.WriteLine("My cuurent Thread" + Thread.CurrentThread.Name);
+Console.ReadLine();
+
+
